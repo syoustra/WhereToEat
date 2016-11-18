ff05edc54b525f5b64e89d43c06b491d52b28180
syoustra <steph.you07@gmail.com>
2016-11-17 21:36:57 -0500

    Neatening random.php file

--- random.php
+++ random.php
@@ -4,13 +4,12 @@ include("inc/header.php");
 include("inc/connection.php");
 
 
-?>
-<h1>EAT UP!!!!!</h1>
 
-<!--SHOW RANDOM RESTAURANT SELECTION HERE-->
-<!--Google tells me there are faster/better ways of doing this. By the time the database gets big enough for those concerns to be an issue, I should be able to understand and implement them.-->
+echo "<h1>EAT UP!!!!!</h1>";
 
-<?php 
+// SHOW RANDOM RESTAURANT SELECTION HERE
+// Google tells me there are faster/better ways of doing this. By the time the database gets big enough for those concerns to be an issue, I should be able to understand and implement them.
+
 
 //CHOOSES SINGLE RANDOMIZED ELEMENT
 $random = $db->query('SELECT cuisine FROM cuisine ORDER BY RAND() LIMIT 1');
@@ -19,6 +18,5 @@ $choice = $random->fetch();
 //DISPLAYS SELECTION
 echo "Then why don't you go eat some " . $choice[0] . "?";
 
-?>
 
-<?php include("inc/footer.php"); ?>
+include("inc/footer.php"); ?>
