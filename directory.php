5666bdd6b1234ca204bac85eaafa7c344a8ce1fe
syoustra <steph.you07@gmail.com>
2016-11-18 03:37:12 -0500

    Added header to directory table

--- directory.php
+++ directory.php
@@ -17,6 +17,11 @@ include("inc/connection.php");
 $fullListing = $db->query('SELECT * FROM restaurants');
 
 echo "<table>";
+//TABLE HEADINGS
+echo "<tr><td>Name</td><td>Neighborhood</td><td>Cuisine</td><td>NewToMe?</td></tr>";
+
+
+//FILL IN CONTENTS OF DATABASE
 	while ($dir = $fullListing->fetch())
 	{
 		echo "<tr><td>" . $dir['rest_name'] . "</td><td>" . $dir['neighborhood_id'] . "</td><td>" . $dir['cuisine_id'] . "</td><td>" . $dir['new_to_me'] . "</td></tr>";
