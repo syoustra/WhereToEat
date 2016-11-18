65fb4313768af11bba3ea550717629bc44e29b37
syoustra <steph.you07@gmail.com>
2016-11-18 04:01:39 -0500

    Finished inner joins on directory page

--- directory.php
+++ directory.php
@@ -14,7 +14,10 @@ include("inc/connection.php");
 <?php
 
 
-$fullListing = $db->query('SELECT * FROM restaurants INNER JOIN neighborhood ON restaurants.neighborhood_id = neighborhood.neighborhood_id');
+$fullListing = $db->query('SELECT * FROM restaurants 
+	INNER JOIN neighborhood ON restaurants.neighborhood_id = neighborhood.neighborhood_id
+	INNER JOIN cuisine ON restaurants.cuisine_id = cuisine.cuisine_id'
+	);
 
 echo "<table>";
 //TABLE HEADINGS
@@ -24,7 +27,7 @@ echo "<tr><td>Name</td><td>Neighborhood</td><td>Cuisine</td><td>NewToMe?</td></t
 //FILL IN CONTENTS OF DATABASE
 	while ($dir = $fullListing->fetch())
 	{
-		echo "<tr><td>" . $dir['rest_name'] . "</td><td>" . $dir['neighborhood_name'] . "</td><td>" . $dir['cuisine_id'] . "</td><td>" . $dir['new_to_me'] . "</td></tr>";
+		echo "<tr><td>" . $dir['rest_name'] . "</td><td>" . $dir['neighborhood_name'] . "</td><td>" . $dir['cuisine'] . "</td><td>" . $dir['new_to_me'] . "</td></tr>";
 	}
 
 	echo "</table>";
