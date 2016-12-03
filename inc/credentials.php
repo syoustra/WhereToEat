fed8fd2768acbc48e03636aa4473ee22c59e4ac7
syoustra <steph.you07@gmail.com>
2016-12-03 15:57:42 -0500

    ONE MORE TRY .... resetting blank gitignore and credentials

---
+++ inc/credentials.php
@@ -0,0 +1,12 @@
+<?php
+
+//File containing the PDO credentials; part of .gitignore for security's sake
+// $db = new PDO("mysql:host=HOSTNAME;dbname=database;port=3306", "USERNAME", "PASSWORD");
+
+	$host = 'INSERT_HOSTNAME_HERE';
+	$db = 'restaurantDB';
+	$user = 'INSERT_USERNAME_HERE';
+	$pass = 'INSERT_PASSWORD_HERE';
+
+
+
